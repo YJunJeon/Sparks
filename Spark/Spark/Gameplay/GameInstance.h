9f3c4e183e4a088bf7b637adc688971ca2faecac
Yejun June <s231050@students.aie.edu.au>
2024-05-22 16:04:58 +1000

    initial commit

---
+++ Spark/Spark/Gameplay/GameInstance.h
@@ -0,0 +1,43 @@
+#pragma once
+
+class Config;
+class Screen;
+class LevelManager;
+
+class GameInstance
+{
+	friend class Application;
+	friend class LevelManager;
+
+public:
+	GameInstance(const GameInstance&) = delete;
+	GameInstance(GameInstance&&) = delete;
+
+public:
+	GameInstance& operator=(const GameInstance&) = delete;
+	GameInstance& operator=(GameInstance&&) = delete;
+
+protected:
+	GameInstance();
+	virtual ~GameInstance();
+
+protected:
+	virtual void BeginPlay();
+	virtual void EndPlay();
+
+	Config* GetConfig() const;
+	Screen* GetScreen() const;
+	LevelManager* GetLevelManager() const;
+
+private:
+	Config* m_config; // Owned anc controlled by the Application
+	Screen* m_screen; // Owned anc controlled by the Application
+	LevelManager* m_levelManager;
+
+private:
+	void Init();
+	void Tick(float _dt) const;
+	void Render() const;
+
+};
+
